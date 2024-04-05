5c0fef3b7e7faa069fe6ae77a3cfd999253c5c95
Diego Betto <diego@papion.it>
2024-04-05 17:50:37 +0200

    gestione concessionaria

--- scriptOggetti.js
+++ scriptOggetti.js
@@ -34,7 +34,6 @@ const FnAutomobili = (ruote, nome, alim, suono, nrPass, id) => {
     }
 }
 
-const auto = FnAutomobili(3, 'Ford', 'diesel', 'brum', 1, 'FRD01')
 /*
 console.log(auto.incrementaPass())
 console.log(auto.decrementaPass())
@@ -52,7 +51,52 @@ console.log(auto.id)*/
     const Concessionaria = FnConcessionaria(..)
 
     console.log(Concessionaria.numeroMezzi())
-    console.log(Concessionaria.aggiungi(auto1))
-    console.log(Concessionaria.aggiungi(auto2))
-    console.log(Concessionaria.esiste('idauto'))
+    console.log(Concessionaria.aggiungiMezzo(auto1))
+    console.log(Concessionaria.aggiungiMezzo(auto2))
+    console.log(Concessionaria.esisteMezzo('idauto'))
 */
+
+const FnConcessionaria = () => {
+    // elenco mezzi della concessionaria
+    const mezzi = [];
+
+    return {
+        // ritorno la lunghezza del magazzino, quanti mezzi ho
+        numeroMezzi: () => mezzi.length,
+        // aggiunge un mezzo al magazzino
+        aggiungiMezzo: (mezzo) => {
+            mezzi.push(mezzo)
+        },
+        // controlla se esiste l'id del mezzo tra i vari mezzi
+        // del magazzino
+        esisteMezzo: (idCercato) => {
+            // idxMezzo sarà >= 0 se c'è il mezzo
+            // sarà -1 se non c'è
+            const idxMezzo = mezzi.findIndex(function(m){
+                // idCercato è uguale al id dell'auto corrente?
+                // se si torna true, e findIndex si ferma tornandomi
+                // la posizione dell'auto trovata
+                // se no torna false, e findIndex mi restituirà -1
+                return m.id === idCercato
+            })
+
+            // const idxMezzo = mezzi.findIndex((m) => m.id === idCercato) >= 0
+            // come se fosse
+            // if (idxMezzo >=0) { return true } else {return false}
+            return idxMezzo >= 0
+        },
+    }
+}
+
+const Concessionaria = FnConcessionaria()
+
+const auto1 = FnAutomobili(4, 'Ford', 'diesel', 'brum', 1, 'FRD01')
+const auto2 = FnAutomobili(3, 'Skoda', 'benzina', 'bru', 2, 'SKD01')
+
+console.log(Concessionaria.numeroMezzi())
+console.log(Concessionaria.aggiungiMezzo(auto1))
+console.log(Concessionaria.aggiungiMezzo(auto2))
+console.log(Concessionaria.numeroMezzi())
+
+console.log(Concessionaria.esisteMezzo('FRD01'))
+console.log(Concessionaria.esisteMezzo('FRD22'))
