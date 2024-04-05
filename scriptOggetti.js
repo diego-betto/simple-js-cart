cc52f74a83e1956e8542cb2ee3298b6e06453548
Diego Betto <diego@papion.it>
2024-04-05 17:05:30 +0200

    esercizio concessionaria

--- scriptOggetti.js
+++ scriptOggetti.js
@@ -17,3 +17,42 @@ console.log(auto.numeroPasseggeri())
 console.log(auto.id)
 
 */
+
+
+const FnAutomobili = (ruote, nome, alim, suono, nrPass, id) => {
+    let nrPasseg = nrPass
+
+    return {
+        ruote,
+        nome,
+        alim,
+        suono,
+        id,
+        incrementaPass: () => nrPasseg++,
+        decrementaPass: () => nrPasseg > 0 ? --nrPasseg : nrPasseg,
+        numeroPasseggeri: () => nrPasseg,
+    }
+}
+
+const auto = FnAutomobili(3, 'Ford', 'diesel', 'brum', 1, 'FRD01')
+/*
+console.log(auto.incrementaPass())
+console.log(auto.decrementaPass())
+console.log(auto.numeroPasseggeri())
+console.log(auto.id)*/
+
+/*
+    creare un costruttore di oggetti FnConcessionaria
+    - quanti automezzi ha in sede
+    - aggiungere un automezzo
+    - c'è l'automezzo nella sede
+
+    const FnConcessionaria = (..) => {}
+
+    const Concessionaria = FnConcessionaria(..)
+
+    console.log(Concessionaria.numeroMezzi())
+    console.log(Concessionaria.aggiungi(auto1))
+    console.log(Concessionaria.aggiungi(auto2))
+    console.log(Concessionaria.esiste('idauto'))
+*/
