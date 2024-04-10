9787df0027477ab8db83677aa9ef586ff1729e54
Diego Betto <diego@papion.it>
2024-04-10 16:00:23 +0200

    esercizio 2

--- esercizio.js
+++ esercizio.js
@@ -32,6 +32,52 @@ Espone:
  - metodo statoRiscaldamento() // 'acceso' o 'spento'?
 */
 
+const FnCasa = (indirizzo, numero, superficie) => {
+    let riscaldamentoAcceso = false;
+
+    return {
+        indirizzo: indirizzo,
+        numero: numero, 
+        superficie: superficie,
+        accendiRiscaldamento: () => {
+            riscaldamentoAcceso = true;
+        },
+        spegniRiscaldamento: () => {
+            riscaldamentoAcceso = false;
+        },
+        switchaRiscaldamento: () => {
+            riscaldamentoAcceso = !riscaldamentoAcceso;
+        },
+        statoRiscaldamento: () => riscaldamentoAcceso ? 'acceso' : 'spento'        
+    }
+}
+
+const casa1 = FnCasa('via di Qua', 69, 90);
+
+console.log(casa1.statoRiscaldamento())
+/*
+console.log(casa1.accendiRiscaldamento()) // accende, cambia lo stato ad acceso
+console.log(casa1.statoRiscaldamento()) // output 'acceso'
+
+console.log(casa1.spegniRiscaldamento()) // spegne, cambia lo stato a spento
+console.log(casa1.statoRiscaldamento()) // output 'acceso'
+
+console.log(casa1.switchaRiscaldamento()) // era spento, accende
+console.log(casa1.statoRiscaldamento()) // output 'acceso'
+console.log(casa1.switchaRiscaldamento()) // era acceso, spegne
+console.log(casa1.statoRiscaldamento()) // output 'spento'
+
+console.log(casa1.accendiRiscaldamento()) // era spento, accende
+console.log(casa1.statoRiscaldamento()) // output 'acceso'
+console.log(casa1.accendiRiscaldamento()) // era acceso, accende comunque
+console.log(casa1.statoRiscaldamento()) // output 'acceso'
+
+console.log(casa1.spegniRiscaldamento()) // era acceso, spegne
+console.log(casa1.statoRiscaldamento()) // output 'spento'
+console.log(casa1.spegniRiscaldamento()) // era spento, spengo comunque
+console.log(casa1.statoRiscaldamento()) // output 'spento'
+*/
+
 /*
 Esercizio 3
 Scrivere una funzione per generare un oggetto inquilino.
