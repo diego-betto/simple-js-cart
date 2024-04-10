4cbc29ca6d10eff52c94bcdace9c2da3247e1151
Diego Betto <diego@papion.it>
2024-04-10 14:47:55 +0200

    Esercizi

---
+++ esercizio.js
@@ -0,0 +1,33 @@
+/*
+Esercizio 1
+Scrivere una funzione JS SommaNumeri() che preso un valore, 
+fa la somma da 1 a quel valore e me la restituisce
+
+console.log(SommaNumeri(5)) // 15
+*/
+
+/*
+Esercizio 2
+Scrivere una funzione per generare un oggetto casa.
+Ricevere parametri:
+ - indirizzo
+ - numero porte
+ - superficie in mq
+
+Espone:
+ - metodo accendiRiscaldamento() 
+ - metodo spegniRiscaldamento()
+ - metodo switchaRiscaldamento()
+ - metodo statoRiscaldamento() // 'acceso' o 'spento'?
+*/
+
+/*
+Esercizio 3
+Scrivere una funzione per generare un oggetto inquilino.
+Riceve parametri:
+ - nome
+ - anno di nascita
+
+Espone:
+ - metodo saluta() // 'Ciao sono <nome>, ho N anni'
+*/
